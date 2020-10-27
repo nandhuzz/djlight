332cf2fb2c7d55ef21dc907f58c03309021127d8
Anandhu <anandhun@qburst.com>
2020-10-27 21:36:35 +0530

    Added All color

---
+++ script.js
@@ -0,0 +1,16 @@
+function generateColor() {
+    var length = 6,
+        charset = "abcdef0123456789",
+        retVal = "#";
+        for (var i = 0, n = charset.length; i < length; ++i) {
+             retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
+
+function myFunction() 
+{
+    var a = generateColor();
+    document.body.style.backgroundColor = a;
+}
+setInterval("myFunction()", 200);
